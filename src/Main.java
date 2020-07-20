8068fa7cb004d2c39248fbf8c5e4ffcca1598a8e
Phil Lepanto <lepanto@pnw.beer>
2020-07-20 11:22:32 -0700

    Workign through the code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
         
+       
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Kayla";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        String studentLastName = "Hamon";
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
     }
 }
